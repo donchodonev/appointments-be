555b9cf18a5d997baa5305a7a1933b923e5cdaa4
doncho.donev <doncho.donev@mentormate.com>
2023-01-31 13:40:25 +0200

    add authorize attribute to api controller

--- Appointments/Appointments/Controllers/WeatherForecastController.cs
+++ Appointments/Appointments/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Appointments.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
