ce9388c437e786141f6b413b0c67449fab344117
doncho.donev <doncho.donev@mentormate.com>
2023-02-01 13:35:36 +0200

    update ClaimResponse

--- Appointments/SignInUpProxy/ClaimResponse.cs
+++ Appointments/SignInUpProxy/ClaimResponse.cs
@@ -1,13 +1,9 @@
-﻿using System.Reflection;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace SignInUpProxy
 {
     public class ClaimResponse : ContinuationResponse
     {
-        [JsonPropertyName("version")]
-        public string Version => Assembly.GetEntryAssembly().GetName().Version.ToString();
-
         [JsonPropertyName("appRole")]
         public string AppRole { get; set; }
     }
