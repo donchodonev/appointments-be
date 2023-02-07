de2297fed4207af357e236fec5a7230518769dff
doncho.donev <doncho.donev@mentormate.com>
2023-02-07 16:05:20 +0200

    update attribute

--- Appointments/Appointments/Controllers/WeatherForecastController.cs
+++ Appointments/Appointments/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Appointments.Controllers
 {
-    [Authorize(Policy = "AdminOnly")]
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
