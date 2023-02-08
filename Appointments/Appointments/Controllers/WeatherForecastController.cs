12e3b6e81c0cf6393acade18c3f8d875165c7898
doncho.donev <doncho.donev@mentormate.com>
2023-02-08 19:03:06 +0200

    remove logger and added more relaxed policy for now

--- Appointments/Appointments/Controllers/WeatherForecastController.cs
+++ Appointments/Appointments/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using static Appointments.Constants;
 
 namespace Appointments.Controllers
 {
-    [Authorize(AdminOnlyPolicyName)]
+    [Authorize(ProviderOrAdminPolicyName)]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -14,13 +14,6 @@ namespace Appointments.Controllers
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
